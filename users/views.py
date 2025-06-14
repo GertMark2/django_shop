9c00153581e1ae29a42efeac503ac016c59509b4
GertMark2 <marksai07001@gmail.com>
2025-06-14 15:58:45 +0500

    Profile

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -44,8 +44,18 @@ def registration(request):
 
 
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST,
+                           instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title': ' Home - Кабинет'
+        'title': ' Home - Кабинет',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
